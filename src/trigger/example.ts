efa43f9c37650ff93fe508eef6c2501819edf037
CarpMadMan <matthews.lacrosse@gmail.com>
2026-03-22 14:13:05 -0700

    chore: remove old trigger.dev task files

--- src/trigger/example.ts
+++
@@ -1,8 +0,0 @@
-import { task } from "@trigger.dev/sdk";
-
-export const helloWorldTask = task({
-  id: "hello-world",
-  run: async (payload: string) => {
-    return { message: `Hello ${payload}!` };
-  },
-});
